a21b608c4c6d9424bb7350758e46ce46fc1c008f
hmogensen <hpmogen@gmail.com>
2014-04-10 14:39:59 +0200

    Bug fix in triggerpanel
    One more file

---
+++ sensorimotor/gui/@ScGuiManager/new_waveform.m
@@ -0,0 +1,43 @@
+function new_waveform(obj)
+
+obj.zoom_on = false; obj.pan_on = false;
+dlg = figure;
+set(dlg,'windowstyle','modal');
+setheight(dlg,60);
+setwidth(dlg,200);
+dlgmgr = ScLayoutManager(dlg);
+dlgmgr.newline;
+dlgmgr.add(getuitext(['Signal: ' obj.signal.tag]),200);
+dlgmgr.newline;
+dlgmgr.add(getuitext('Name:'),40);
+ui_waveform_tag = dlgmgr.add(uicontrol('style','edit','string',...
+    [],'horizontalalignment','left'),160);
+
+dlgmgr.newline;
+dlgmgr.add(uicontrol('style','pushbutton','string',...
+    'Add','callback',@add_waveform_callback),100);
+
+    function add_waveform_callback(~,~)
+        tag = deblank(get(ui_waveform_tag,'string'));
+        if ~isempty(tag) && ~obj.signal.waveforms.has('tag',tag)
+            obj.signal.waveforms.add(ScWaveform(obj.signal,tag,[]));
+            set(obj.waveformpanel,'Visible','on');
+            signal_callback;
+            obj.has_unsaved_changes = true;
+            resize_fcn = get(obj.current_view,'ResizeFcn');
+            resize_fcn();
+        else
+            msgbox(['Waveform name must be non-empty and unique for '...
+                'signal ' obj.signal.tag '.']);
+        end
+        close(dlg);
+    end
+
+dlgmgr.add(uicontrol('style','pushbutton','string',...
+    'Cancel','callback',@cancel_callback),100);
+
+    function cancel_callback(~,~)
+        close(dlg);
+    end
+
+end

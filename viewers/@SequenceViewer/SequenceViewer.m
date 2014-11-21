5a9293a03d30dad482530bfce992c6fdd8b46426
Hannes Mogensen <hpmogen@gmail.com>
2014-11-21 14:22:46 +0100

    Updating versioning number

--- viewers/@SequenceViewer/SequenceViewer.m
+++ viewers/@SequenceViewer/SequenceViewer.m
@@ -1,7 +1,7 @@
 classdef SequenceViewer < handle
     methods (Static)
         function str = version_str()
-            str = '>1.0.10';
+            str = '1.0.11';
         end
     end
     properties (SetObservable)

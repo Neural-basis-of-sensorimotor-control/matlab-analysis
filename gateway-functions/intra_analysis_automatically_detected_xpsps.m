3b4a1d25e3333001e7f2a0cfd1099eeb79a2e98c
hmogensen <hpmogen@gmail.com>
2016-10-27 15:59:07 +0200

    Add script for automatic analysis

---
+++ gateway-functions/intra_analysis_automatically_detected_xpsps.m
@@ -0,0 +1,21 @@
+clc
+clear
+
+neurons = get_intra_neurons();
+stims = get_intra_motifs();
+
+response_min = 5e-3;
+response_max = 1e-2;
+remove_fraction = .1;
+
+for i=1:length(neurons)
+	fprintf('%d out of %d\n', i, length(neurons));
+	
+	neuron = neurons(i);
+	signal = sc_load_signal(neuron, 'check_raw_data_dir', true);
+	
+	signal.reset_amplitudes();
+	signal.update_amplitudes(response_min, response_max, remove_fraction);
+	signal.sc_save(false);
+end
+

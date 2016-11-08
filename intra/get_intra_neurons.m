73589228e566b81f88b266b222255f0d42106eec
Hannes Mogensen <hpmogen@gmail.com>
2016-11-08 12:22:32 +0100

    Update intra neurons list

--- intra/get_intra_neurons.m
+++ intra/get_intra_neurons.m
@@ -1,34 +1,34 @@
 function neurons = get_intra_neurons(indx)
+%Experiment file, Raw data file tag, Channel tag, time start, time stop, xPSP template
 
 data ={
-  'IHNR_sc.mat',    'IHNR0000',   'patch',  -inf, inf
-  'HPNR_sc.mat',    'HPNR0003',   'patch2', -inf, inf
-  'IKNR_sc.mat',    'IKNR0000',   'patch',  -inf, inf
-  'IFNR_sc.mat',    'IFNR0004',   'patch',  -inf, inf
-  'ILNR_sc.mat',    'ILNR0001',   'patch',  -inf, inf
-  'IFNR_sc.mat',    'IFNR0002',   'patch',  -inf, inf
-  'IHNR_sc.mat',    'IHNR0001',   'patch',  -inf, inf
-  'ICNR_sc.mat',    'ICNR0003',   'patch',  -inf, inf
-  'ICNR_sc.mat',    'ICNR0002',   'patch',  -inf, inf
-  'HRNR_sc.mat',    'HRNR0000',   'patch2', -inf, inf
- % 'BENR_sc.mat',    'BENR0005',   'patch',  -inf, inf Removed because of
- % low PSP response to single pulse stimulation (only dendrite spikes)
-  'BJNR_sc.mat',    'BJNR0005',   'patch',  -inf, inf
-  'BONR_sc.mat',    'BONR0009',   'patch',  -inf, inf
-  'BONR_sc.mat',    'BONR0006',   'patch',  -inf, inf
-  'CCNR_sc.mat',    'CCNR0000',   'patch',  235, 580
-  'CANR_sc.mat',    'CANR0008',   'patch',  -inf, inf
-  'CANR_sc.mat',    'CANR0001',   'patch',  -inf, inf
-  'BPNR_sc.mat',    'BPNR0000',   'patch',  -inf, inf
-  'BPNR_sc.mat',    'BPNR0001',   'patch',  -inf, inf
-  'BNNR_sc.mat',    'BNNR0007',   'patch',  -inf, inf
-  'BKNR_sc.mat',    'BKNR0002',   'patch',  -inf, inf
-  'BKNR_sc.mat',    'BKNR0000',   'patch',  -inf, inf
-  'BHNR_sc.mat',    'BHNR0002',   'patch',  -inf, inf
-  'BGNR_sc.mat',    'BGNR0000',   'patch',  -inf, inf
-  'DJNR_sc.mat',    'DJNR0005',   'patch',  900,  1800
-  'BPNRtst_sc.mat', 'BPNR0000',   'patch',  -inf, inf
-  'BPNRtst_sc.mat', 'BPNR0001',   'patch',  -inf, inf};
+%  'BENR_sc.mat',    'BENR0005',   'patch',  -inf, inf
+%  'BGNR_sc.mat',    'BGNR0000',   'patch',  -inf, inf
+%  'BHNR_sc.mat',    'BHNR0002',   'patch',  -inf, inf
+  'BJNR_sc.mat',    'BJNR0005',   'patch',  -inf, inf,  'EPSP5'
+%  'BKNR_sc.mat',    'BKNR0000',   'patch',  -inf, inf
+  'BKNR_sc.mat',    'BKNR0002',   'patch',  430,  1200,  'EPSP5'
+%  'BNNR_sc.mat',    'BNNR0007',   'patch',  -inf, inf
+  'BONR_sc.mat',    'BONR0006',   'patch',  -inf, inf,  'EPSP5'
+  'BONR_sc.mat',    'BONR0009',   'patch',  -inf, inf,  'EPSP5'
+  'BPNRtst_sc.mat', 'BPNR0000',   'patch',  -inf, inf,  'EPSP5' 
+  'BPNRtst_sc.mat', 'BPNR0001',   'patch',  -inf, inf,  'EPSP5'
+  'CANR_sc.mat',    'CANR0001',   'patch',  -inf, inf,  'EPSP5'
+  'CANR_sc.mat',    'CANR0008',   'patch',  -inf, inf,  'EPSP5'
+  'CCNR_sc.mat',    'CCNR0000',   'patch',  235,  580,  'EPSP5'
+  'DJNR_sc.mat',    'DJNR0005',   'patch',  900,  1800, 'EPSP5'
+  'HPNR_sc.mat',    'HPNR0003',   'patch2', -inf, inf,  '?'
+  'HRNR_sc.mat',    'HRNR0000',   'patch2', -inf, inf,  '?'
+  'ICNR_sc.mat',    'ICNR0002',   'patch',  -inf, inf,  '?'
+  'ICNR_sc.mat',    'ICNR0003',   'patch',  -inf, inf,  '?'
+  'IFNR_sc.mat',    'IFNR0002',   'patch',  -inf, inf,  '?'
+  'IFNR_sc.mat',    'IFNR0004',   'patch',  -inf, inf,  '?'
+  'IHNR_sc.mat',    'IHNR0000',   'patch',  -inf, inf,  '?'
+  'IHNR_sc.mat',    'IHNR0001',   'patch',  -inf, inf,  '?'
+  'IKNR_sc.mat',    'IKNR0000',   'patch',  -inf, inf,  '?'
+  'ILNR_sc.mat',    'ILNR0001',   'patch',  -inf, inf,  '?'
+  'IDNR_sc.mat',    'IDNR0001',   'patch',  -inf, inf,  '?'
+  };
 
 for i=size(data,1):-1:1
   neurons(i).expr_file = data{i,1};

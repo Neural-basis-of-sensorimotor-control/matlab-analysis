f05a1c49c428d59cc416cbb0f15b0c7693026e1a
hmogensen <hpmogen@gmail.com>
2016-11-22 03:36:57 +0100

    Update save-experiment.m

--- sensorimotor/classes/@ScExperiment/save_experiment.m
+++ sensorimotor/classes/@ScExperiment/save_experiment.m
@@ -12,6 +12,10 @@ if isempty(save_path) || ~exist(save_path, 'file') || prompt_before_saving
   save_path = fullfile(pname, fname);
 end
 
+[folder, file, extension] = fileparts(which(save_path));
+experiment.sc_dir = folder;
+experiment.save_name = [file extension];
+
 obj = experiment; %#ok<NASGU>
 save(save_path, 'obj')
 saved = true;

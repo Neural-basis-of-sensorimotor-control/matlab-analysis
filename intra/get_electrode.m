facd706bb89f36589dcaab9a678def6924d5da7f
Hannes Mogensen <hpmogen@gmail.com>
2017-06-02 10:21:58 +0200

    Update get_electrode.m

--- intra/get_electrode.m
+++ intra/get_electrode.m
@@ -1,14 +1,29 @@
 function [char_val, num_val] = get_electrode(stim)
 
-if ~ischar(stim)
-  stim = stim.tag;
-end
-
-str = strsplit(stim, '#');
-char_val =  str{2};
-
-if nargout>1
-	num_val = str2double(char_val(2));
+if iscell(stim) || (~ischar(stim) && length(stim) > 1)
+  
+  char_val = cell(size(stim));
+  num_val = nan(size(stim));
+  
+  for i=1:length(stim)
+    [tmp_char_val, num_val(i)] = get_electrode(get_item(stim, i));
+    
+    char_val(i) = {tmp_char_val};
+  end
+  
+else
+  
+  if ~ischar(stim)
+    stim = stim.tag;
+  end
+  
+  str = strsplit(stim, '#');
+  char_val =  str{2};
+  
+  if nargout>1
+    num_val = str2double(char_val(2));
+  end
+  
 end
 
 end

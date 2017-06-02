fefa5ec2e3c88ce992ca71e3e45342cbdd282944
Hannes Mogensen <hpmogen@gmail.com>
2017-06-02 10:25:28 +0200

    Add support for char input to count_items_in_list()

--- util/list-operators/count_items_in_list.m
+++ util/list-operators/count_items_in_list.m
@@ -4,17 +4,23 @@ if nargin>1
   list = get_values(list, property);
 end
 
-unique_vals = unique(list);
+if ischar(list)
+  unique_vals = list;
+  counts = 1;
+else
+  unique_vals = unique(list);
 
-counts = nan(size(unique_vals));
+  counts = nan(size(unique_vals));
 
-for i=1:length(counts)
+  for i=1:length(counts)
+    
+    counts(i) = nnz(equals(list, get_item(unique_vals,i)));
   
-  counts(i) = nnz(equals(list, get_item(unique_vals,i)));
-  
+  end
+
+  [counts, indx] = sort(counts, 'descend');
+  unique_vals = unique_vals(indx);
+
 end
-
-[counts, indx] = sort(counts, 'descend');
-unique_vals = unique_vals(indx);
 
 end

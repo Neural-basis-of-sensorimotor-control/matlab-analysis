0818d19fd0b5dd6aee449209dbfda07b489e188c
Hannes Mogensen <hpmogen@gmail.com>
2017-08-28 13:39:05 +0200

    Update check for experiment file

--- sc/experiment/classes/@ScExperiment/load_experiment.m
+++ sc/experiment/classes/@ScExperiment/load_experiment.m
@@ -1,30 +1,60 @@
 function experiment = load_experiment(full_file_path)
 
 if ~isfile(full_file_path)
-  temp_full_file_path = [get_default_experiment_dir() full_file_path];
+  
+  temp_full_file_path = [full_file_path '.mat'];
   
   if isfile(temp_full_file_path)
-    full_file_path = temp_full_file_path;
-  else
-    [filename, pathname] = uigetfile(sprintf('*%s%s', filesep, temp_full_file_path), ...
-      sprintf('Select directory for %s', temp_full_file_path));
     
-    temp_full_file_path = fullfile(pathname, filename);
+    full_file_path = temp_full_file_path;
+    
+  else
+    
+    temp_full_file_path = [get_default_experiment_dir() full_file_path];
     
     if isfile(temp_full_file_path)
+      
       full_file_path = temp_full_file_path;
-      set_default_experiment_dir(pathname);
+      
     else
-      error('Could not find file %s', full_file_path);
+      
+      temp_full_file_path = [get_default_experiment_dir() full_file_path '.mat'];
+      
+      if isfile(temp_full_file_path)
+        
+        full_file_path = temp_full_file_path;
+        
+      else
+        
+        [filename, pathname] = uigetfile(full_file_path, ...
+          sprintf('Select directory for %s', full_file_path));
+        
+        temp_full_file_path = fullfile(pathname, filename);
+        
+        if isfile(temp_full_file_path)
+          
+          full_file_path = temp_full_file_path;
+          set_default_experiment_dir(pathname);
+          
+        else
+          
+          error('Could not find file %s', full_file_path);
+          
+        end
+        
+      end
+      
     end
+    
   end
+  
 end
 
 d = load(full_file_path, 'obj');
 
-[~, name] = fileparts(full_file_path);
+[~, name, ext] = fileparts(full_file_path);
 experiment = d.obj;
-experiment.save_name = name;
+experiment.save_name = [name ext];
 
 set_last_experiment(full_file_path);
 

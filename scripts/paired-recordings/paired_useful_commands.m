d02a986eb8460b23b319d5e5a1da777b80109a46
Hannes Mogensen <hpmogen@gmail.com>
2017-09-12 11:11:41 +0200

    Add useful commands to paired recordings analysis

---
+++ scripts/paired-recordings/paired_useful_commands.m
@@ -0,0 +1,8 @@
+close all
+paired_load_constants
+
+paired_plot_ic_signal(ic_neurons, ic_pretrigger, ic_posttrigger, ic_t_epsp_range, ic_t_spike_range)
+paired_plot_unitary_epsp_response(ic_neurons, ic_pretrigger, ic_posttrigger, ic_t_epsp_range, ic_t_spike_range);
+
+mxfigs
+brwfigs

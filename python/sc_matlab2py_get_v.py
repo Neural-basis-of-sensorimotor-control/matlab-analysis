55196131fea1c0b00470e2b05768c50f2bd4cca3
Hannes Mogensen <hmogensen@users.noreply.github.com>
2018-01-19 15:27:35 +0100

    Update sc_matlab2py_get_v.py

--- python/sc_matlab2py_get_v.py
+++ python/sc_matlab2py_get_v.py
@@ -3,4 +3,4 @@ import matlab.engine
 def sc_matlab2py_get_v(experiment_filename, file_tag, signal_tag):
 
 	eng = matlab.engine.start_matlab()
-  return eng.sc_matlab2py_get_v(experiment_filename, file_tag, signal_tag)
+	return eng.sc_matlab2py_get_v(experiment_filename, file_tag, signal_tag)

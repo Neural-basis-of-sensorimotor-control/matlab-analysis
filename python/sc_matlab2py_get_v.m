eb9b0b5fee708f34ee8aa8028dc3a1131c1d5118
Hannes Mogensen <hpmogen@gmail.com>
2018-01-22 14:35:36 +0100

    Update Python wrapper

--- python/sc_matlab2py_get_v.m
+++ python/sc_matlab2py_get_v.m
@@ -1,9 +1,23 @@
-function v = sc_matlab2py_get_v(experiment_filename, file_tag, signal_tag)
+function v = sc_matlab2py_get_v(experiment_filepath, file_filepath, channel_tag)
 
-neuron = ScNeuron('experiment_filename', experiment_filename, ...
-  'file_tag', file_tag, 'signal_tag', signal_tag);
+experiment_filepath = strrep(experiment_filepath, '/', filesep);
+experiment_filepath = strrep(experiment_filepath, '\', filesep);
 
-signal = sc_load_signal(neuron);
+file_filepath       = strrep(file_filepath, '/', filesep);
+file_filepath       = strrep(file_filepath, '\', filesep);
+
+[pathstr, file_tag] = fileparts(file_filepath);
+
+neuron = ScNeuron('experiment_filename', experiment_filepath);
+
+expr = sc_load_experiment(neuron);
+expr.set_fdir(pathstr);
+
+file = expr.get('tag', file_tag);
+signal = file.signals.get('tag', channel_tag);
+signal.sc_loadtimes();
+
+signal.update_property_values();
 
 v = signal.get_v(true, true, true, true);
 
--- python/sc_matlab2py_get_v.py
+++ python/sc_matlab2py_get_v.py
@@ -1,6 +1,6 @@
 import matlab.engine
 
-def sc_matlab2py_get_v(experiment_filename, file_tag, signal_tag):
+def sc_matlab2py_get_v(experiment_filepath, file_filepath, signal_tag):
 
-	eng = matlab.engine.start_matlab()
-	return eng.sc_matlab2py_get_v(experiment_filename, file_tag, signal_tag)
+	eng = matlab.engine.connect_matlab()
+	return eng.sc_matlab2py_get_v(experiment_filepath, file_filepath, signal_tag)

75b502eeb855348daff6311bebb6fd74dcb4f385
Hannes Mogensen <hpmogen@gmail.com>
2018-03-17 10:06:57 +0100

    Update paired recordnigs

--- scripts/paired-recordings/paired_generate_triplet_fig.m
+++ scripts/paired-recordings/paired_generate_triplet_fig.m
@@ -2,7 +2,7 @@
 clear
 paired_setup
 
-reset_fig_indx()
+%reset_fig_indx()
 
 pretrigger  = -1;
 posttrigger = 1;
@@ -15,46 +15,48 @@ triplet_file_tags = file_tags(arrayfun(@(y) nnz(arrayfun(@(x) strcmp(x.file_tag,
 
 triplets_list = {};
 
-i=1;
-
-triplets = ec_neurons(arrayfun(@(x) strcmp(x.file_tag, triplet_file_tags{i}), ec_neurons));
-
-triplets_list = add_to_list(triplets_list, triplets);
-
-% incr_fig_indx()
-% clf
-
-for j=1:length(triplets)
+for i=1:length(triplet_file_tags)
   
-  tmp_triplet_1 = triplets(j);
-  [t1, t2] = paired_get_neuron_spiketime(tmp_triplet_1);
+  triplets = ec_neurons(arrayfun(@(x) strcmp(x.file_tag, triplet_file_tags{i}), ec_neurons));
   
-  %sc_square_subplot(2*length(triplets), 2*j-1);
+  triplets_list = add_to_list(triplets_list, triplets);
+  
   incr_fig_indx()
   clf
-  hold on
   
-  sc_kernelhist(t1, t2, pretrigger, posttrigger, kernelwidth, binwidth);
-  [~, ~, h_plot] = sc_kernelhist(t1, t2, pretrigger, posttrigger, 10*kernelwidth, binwidth);
-  set(h_plot, 'LineWidth', 2);
-  [freq_hist, bintimes] = sc_perifreq(t1, t2, pretrigger, posttrigger, 10*binwidth);
-  bar(bintimes, freq_hist)
-  %grid on
-  axis tight
-  title([tmp_triplet_1.file_tag ': ' tmp_triplet_1.template_tag{1} ' - ' tmp_triplet_1.template_tag{2}])
-  
-  %sc_square_subplot(2*length(triplets), 2*j-1);
-  incr_fig_indx()
-  clf
-  hold on
-  sc_kernelhist(t2, t1, pretrigger, posttrigger, kernelwidth, binwidth);
-  [~, ~, h_plot] = sc_kernelhist(t2, t1, pretrigger, posttrigger, 10*kernelwidth, binwidth);
-  set(h_plot, 'LineWidth', 2);
-  [freq_hist, bintimes] = sc_perifreq(t2, t1, pretrigger, posttrigger, 10*binwidth);
-  bar(bintimes, freq_hist)
-  %grid on
-  axis tight
-  title([tmp_triplet_1.file_tag ': ' tmp_triplet_1.template_tag{2} ' - ' tmp_triplet_1.template_tag{1}])
+  for j=1:length(triplets)
+    
+    tmp_triplet_1 = triplets(j);
+    [t1, t2] = paired_get_neuron_spiketime(tmp_triplet_1);
+    
+    sc_square_subplot(2*length(triplets), 2*j-1);
+    %incr_fig_indx()
+    %clf
+    hold on
+    
+    sc_kernelhist(t1, t2, pretrigger, posttrigger, kernelwidth, binwidth);
+    [~, ~, h_plot] = sc_kernelhist(t1, t2, pretrigger, posttrigger, 10*kernelwidth, binwidth);
+    set(h_plot, 'LineWidth', 2);
+    [freq_hist, bintimes] = sc_perifreq(t1, t2, pretrigger, posttrigger, 10*binwidth);
+    bar(bintimes, freq_hist)
+    %grid on
+    axis tight
+    title([tmp_triplet_1.file_tag ': ' tmp_triplet_1.template_tag{1} ' - ' tmp_triplet_1.template_tag{2}])
+    
+    sc_square_subplot(2*length(triplets), 2*j);
+    %incr_fig_indx()
+    %clf
+    hold on
+    sc_kernelhist(t2, t1, pretrigger, posttrigger, kernelwidth, binwidth);
+    [~, ~, h_plot] = sc_kernelhist(t2, t1, pretrigger, posttrigger, 10*kernelwidth, binwidth);
+    set(h_plot, 'LineWidth', 2);
+    [freq_hist, bintimes] = sc_perifreq(t2, t1, pretrigger, posttrigger, 10*binwidth);
+    bar(bintimes, freq_hist)
+    %grid on
+    axis tight
+    title([tmp_triplet_1.file_tag ': ' tmp_triplet_1.template_tag{2} ' - ' tmp_triplet_1.template_tag{1}])
+    
+  end
   
 end
 

0a2bf0400635014013ece0302cab50686a92bdc6
Hannes Mogensen <hpmogen@gmail.com>
2018-03-24 10:08:24 +0100

    Add empty lines

--- sc/util/functions/equals.m
+++ sc/util/functions/equals.m
@@ -5,25 +5,33 @@ function val = equals(x1, x2)
 % x1 and x2 may contain values that are numeric, characters or objects
 
 if isnumeric(x2) || islogical(x2)
+  
   if iscell(x1)
     val = cellfun(@(x) x == x2, x1);
   else
     val = x1 == x2;
   end
+  
 elseif ischar(x2)
+  
   if iscell(x1)
     val = cellfun(@(x) strcmp(x, x2), x1);
   else
     val = strcmp(x1, x2);
   end
+  
 elseif isobject(x2)
+  
   if iscell(x1)
     val = cellfun(@(x) logical(x == x2), x1);
   else
     val = x1 == x2;
   end
+  
 else
+  
   error('equals not implemented for %s', class(x2));
+  
 end
 
 if size(x1,1) ~= size(val,1)

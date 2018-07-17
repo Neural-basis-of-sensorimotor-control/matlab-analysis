52ddc07a8357b17087fb1b23f445c43d074a25f8
Jorntell-lab <hannes.mogensen@med.lu.se>
2018-07-17 14:55:13 +0200

    Remove automatic calculation of pseduo amplitudes in EPSP analysis mode, change display param from latency -> time to peak

--- sc/viewer/classes/panels/@AmplitudeComputedChannel/AmplitudeComputedChannel.m
+++ sc/viewer/classes/panels/@AmplitudeComputedChannel/AmplitudeComputedChannel.m
@@ -18,18 +18,7 @@ classdef AmplitudeComputedChannel < AmplitudeChannel
       if ~nnz(amplitude.valid_data)  
         return
       end
-
-      start = intra_get_amplitude_pseudo_start(amplitude);
-      stop = intra_get_amplitude_pseudo_stop(amplitude, obj.gui.main_channel.v);
-      is_pseudo = ~amplitude.valid_data;
-      rise = intra_get_amplitude_pseudo_rise(start, stop, is_pseudo, ...
-        amplitude, obj.gui.main_channel.v);
-      middle = intra_get_middle_index(rise, .1);
-
-      plot(obj.ax, start(~is_pseudo), rise(~is_pseudo), 'y+', 'Tag', 'Manual', 'LineStyle', 'None');
-      plot(obj.ax, start(is_pseudo), rise(is_pseudo), 'r+', 'Tag', 'Automatic', 'LineStyle', 'None');
-      plot(obj.ax, start(~middle), rise(~middle), 'wo', 'Tag', 'Outliers', 'LineStyle', 'None');
-
+      plot(obj.ax, amplitude.width, amplitude.height, 'y+');
       h = add_legend(obj.ax);
       set(h, 'TextColor', 'w');
     end

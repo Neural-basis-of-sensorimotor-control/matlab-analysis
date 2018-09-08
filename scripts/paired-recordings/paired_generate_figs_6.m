0abe5cb18c910e5af1ed20c7ccc4295365426740
Hannes Mogensen <first_name.last_name@med.lu.se>
2018-09-08 21:32:20 +0200

    Add file with reviewer comments

---
+++ scripts/paired-recordings/paired_generate_figs_6.m
@@ -0,0 +1,11 @@
+clear
+sc_settings.set_current_settings_tag('temporary')
+paired_load_settings();
+paired_load_constants();
+
+% 1A Example triplet = CFNR0003
+
+% Plotta all spTKDE:s, hitta vilka cellpar som motsvarar 3A och koppla till 3B 
+paired_perispike(ec_neurons, ec_pretrigger, ec_posttrigger, ec_kernelwidth, ec_min_stim_latency, ec_max_stim_latency, isi_min_spike_latency);
+%Räkna ut stress för MDS
+paired_mds(ec_neurons); % todo: change from cmdscale to mdscale in function, (the latter outputs stress)

b99693d49d5f1094f8f9eaa67dfe49d3b59d4dbc
Hannes Mogensen <hpmogen@gmail.com>
2018-12-26 01:42:32 +0100

    Update copyright year

--- sc.m
+++ sc.m
@@ -33,7 +33,7 @@ function viewer = sc(varargin)
 %   SC -WHAT print all available information about experiment files in
 %   chosen directory
 %
-%   Copyright 2015 Neural Basis of Sensorimotor Control, Lund University
+%   Copyright 2015-2019 Neural Basis of Sensorimotor Control, Lund University
 %   hannes.mogensen@med.lu.se
 
 github_url = 'https://github.com/Neural-basis-of-sensorimotor-control/matlab-analysis/releases';

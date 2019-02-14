ee5817acbab86b0305cc0c627c1c55bd635a4fca
Hannes Mogensen <hpmogen@gmail.com>
2019-02-14 01:45:07 +0100

    Start afresh with PSP detection paper

---
+++ scripts/hannes-epsp-paper/hamo_generate_figs.m
@@ -0,0 +1,12 @@
+% Load neurons
+
+% Step through stimulation responses, identify EPSP:s
+
+% Extract templates for each method, detect in file
+
+% Compare results
+
+% Evaluate, compare to manual EPSPs ?
+
+% Make table: false positives, false negatives, true positives
+%   ... identification time, computation time, (precision)

162534a50ad3ef7a023297cb4f9435616652ee83
Hannes Mogensen <hpmogen@gmail.com>
2019-02-14 09:38:17 +0100

    Add empty template placeholder

---
+++ +hamo/+templates/@EmptyTemplate/EmptyTemplate.m
@@ -0,0 +1,15 @@
+classdef EmptyTemplate < hamo.templates.Template
+  
+  methods
+    
+    function indx = match_v(varargin)
+      indx = [];
+    end
+    
+    function val = getTriggerTimes(~)
+      val = [];
+    end
+    
+  end
+  
+end
---
+++ +hamo/+templates/@EmptyTemplatePlaceHolder/EmptyTemplatePlaceHolder.m
@@ -0,0 +1,8 @@
+classdef EmptyTemplatePlaceHolder < hamo.templates.EmptyTemplate
+  
+  methods
+    function obj = EmptyTemplatePlaceHolder()
+      obj.tag = '<none>';
+    end
+  end
+end

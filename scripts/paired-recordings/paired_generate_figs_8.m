29894faf4c7814224403da15983efd29f3862361
Hannes Mogensen <hmogensen@users.noreply.github.com>
2019-05-31 08:56:45 +0200

    Add list of paired neurons

---
+++ scripts/paired-recordings/paired_generate_figs_8.m
@@ -0,0 +1,56 @@
+x = {
+    'BKNR_SSSA_sc.mat', 'BKNR0000', 'patch', 'spike1-double'
+    'BMNR_SSSA_sc.mat', 'BMNR0000', 'patch', 'spike2-double'
+    'BMNR_SSSA_sc.mat', 'BMNR0000', 'patch', 'spike13-double'
+    
+    'CENR_sc.mat', 'CENR0001', 'patch', 'spike1-double'
+    'CENR_sc.mat', 'CENR0001', 'patch', 'spike2-double'
+    
+    'CFNR_sc.mat', 'CFNR0003', 'patch', 'spike1-double'
+    'CFNR_sc.mat', 'CFNR0003', 'patch', 'spike2-double'
+    'CFNR_sc.mat', 'CFNR0003', 'patch', 'spike-3'
+    
+    'CNNR_sc.mat', 'CNNR0009', 'patch', 'spike1-double'
+    'CNNR_sc.mat', 'CNNR0009', 'patch', 'spike2-double'
+
+    'DBNR_sc.mat', 'DBNR0003', 'patch', 'spike1-double'
+    'DBNR_sc.mat', 'DBNR0003', 'patch', 'spike2-double'
+
+    
+    'DENR_sc.mat', 'DENR0004', 'patch', 'spike1-double'
+    'DENR_sc.mat', 'DENR0004', 'patch', 'spike2-double'
+    'DENR_sc.mat', 'DENR0004', 'patch', 'spike3-double'
+    
+    'DGNR_sc.mat', 'DGNR0005', 'patch', 'spike2-double'
+    'DGNR_sc.mat', 'DGNR0005', 'patch', 'spike3-double'
+    
+    'DHNR_sc.mat', 'DHNR0002', 'patch', 'spike1-double'
+    'DHNR_sc.mat', 'DHNR0002', 'patch', 'spike2-double'
+    
+    'DHNR_sc.mat', 'DHNR0006', 'patch', 'spike1-double'
+    'DHNR_sc.mat', 'DHNR0006', 'patch', 'spike2-double'
+    'DHNR_sc.mat', 'DHNR0006', 'patch', 'spike3-double'
+
+    
+    'DHNR_sc.mat', 'DHNR0007', 'patch', 'spike1-double'
+    'DHNR_sc.mat', 'DHNR0007', 'patch', 'spike2-double'
+    
+    'DJNR_sc.mat', 'DJNR0005', 'patch2', 'ic-spike-p2-1'
+    'DJNR_sc.mat', 'DJNR0005', 'patch2', 'ec-spike-p2-1'
+    
+    'HRNR_sc.mat', 'HRNR0000', 'patch', 'ic-spike-p1-1'
+    'HRNR_sc.mat', 'HRNR0000', 'patch', 'ec-spike-p1-1'
+
+    
+    'HVNR_sc.mat', 'HVNR0001', 'patch', 'spike1-double'
+    'HVNR_sc.mat', 'HVNR0001', 'patch', 'spike2-double'
+
+    
+    'ICNR_sc.mat', 'ICNR0002', 'patch', 'ec-spike-p1-1'
+    'ICNR_sc.mat', 'ICNR0002', 'patch', 'ic-spike-p1-1'
+
+    
+    'IFNR_sc.mat', 'IFNR0000', 'patch', 'ec-spike-p1-1'
+    'IFNR_sc.mat', 'IFNR0000', 'patch', 'ec-spike-p1-2'
+    
+    };

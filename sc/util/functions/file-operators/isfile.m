c91c08d2fbd6509b799a03d12070d2f27be6401b
Hannes Mogensen <hannes.mogensen@skane.se>
2021-04-06 09:08:48 +0200

    Add isfile check to call builtin function when available

--- sc/util/functions/file-operators/isfile.m
+++ sc/util/functions/file-operators/isfile.m
@@ -1,5 +1,10 @@
 function val = isfile(filepath)
 
-val = ~isempty(filepath) && exist(filepath, 'file') && ~exist(filepath, 'dir');
+if exist('isfile', 'builtin')
+  % Function exists in Matlab >= 2017b
+  val = builtin('isfile', filepath);
+else
+  val = ~isempty(filepath) && exist(filepath, 'file') && ~exist(filepath, 'dir');
+end
 
 end

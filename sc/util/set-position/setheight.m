e3e87cf0f67ba810a70f63267f3b26077ab235bd
Hannes Mogensen <hpmogen@gmail.com>
2023-02-03 13:27:05 +0100

    If height or width < 0, throw warning and skip resizing instead of throwing error

--- sc/util/set-position/setheight.m
+++ sc/util/set-position/setheight.m
@@ -1,5 +1,10 @@
 function setheight(h, height)
 
+if height<0
+    warning('height < 0')
+    return
+end
+
 [x, y, width] = get_position(h);
 
 set_position(h, x, y, width, height);
--- sc/util/set-position/setwidth.m
+++ sc/util/set-position/setwidth.m
@@ -1,5 +1,10 @@
 function setwidth(h, width)
 
+if width<0
+    warning('width < 0')
+    return
+end
+
 [x, y, ~, height] = get_position(h);
 
 set_position(h, x, y, width, height);
